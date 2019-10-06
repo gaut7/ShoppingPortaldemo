c3eceeceb05597f2755293fb951e586158067514
gaut7 <gauravtripathi373@gmail.com>
2019-10-06 10:52:54 +0530

    updated constant file

--- WebContent/js/constants/constant.js
+++ WebContent/js/constants/constant.js
@@ -28,14 +28,7 @@ window.myApp.constant('ASSIGNMENT_CONSTANT', {
 
 window.myApp.constant('ASSIGNMENT_CONSTANT_ERROR_MESSAGES', {
 	
-	ERROR_NEWS_SOURCE :"There is some issue while fetching news data for ",
-	ERROR_WIDGETSETTING_DETAIL :"There is some issue while loading WidgetSetting Detail on portal ",
-	ERROR_OPEN_SOCKETCONNECTION_ERROR :"There is some issue while opening socket connection mirror where ",
-	ERROR_WRONG_MAJOR_MINOR_MESSAGE :"Portal got hit by wrong major,minor value where",
-	ERROR_WRONG_URL_MESSAGE :"Some parameter missing in URL while initializing mirror. Mac-Address of that mirror is ",
-	ERROR_NEWS_APICALL_MESSAGE :"Api called to fetch news data",
-	ERROR_GOOGLE_PHOTO_APICALL_MESSAGE:"The provided Google Album ID does not match any albums",
-	ERROR_APPLE_PHOTO_APICALL_MESSAGE:"Provided link not valid",
+SOMETHING_FAILED :"Something went wrong",
 	
 	
 });
